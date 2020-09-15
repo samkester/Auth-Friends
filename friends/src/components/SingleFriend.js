55bf84bf5814c6524310b10e828c756b171b0031
Sam Kester <samkester@gmail.com>
2020-09-15 16:47:48 -0600

    SingleFriend edits + deletes friend

--- friends/src/components/SingleFriend.js
+++ friends/src/components/SingleFriend.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Link, useParams } from "react-router-dom";
+import { Link, useHistory, useParams } from "react-router-dom";
 import { axiosWithAuth } from "../utils/axiosWithAuth";
 import SingleFriendView from "./SingleFriendView";
 import SingleFriendEdit from "./SingleFriendEdit";
@@ -8,6 +8,8 @@ const SingleFriend = () => {
     const [friend, setFriend] = useState({});
     const [isEditing, setIsEditing] = useState(false);
     const {id} = useParams();
+
+    let history = useHistory();
 
     useEffect(() => {
         axiosWithAuth().get(`/api/friends/${id}`)
@@ -22,16 +24,32 @@ const SingleFriend = () => {
 
     const startEdit = () => {setIsEditing(true)};
 
-    const stopEdit = event => {
+    const saveEdit = event => {
         event.preventDefault();
         // PUT here
-        setIsEditing(false);
+        axiosWithAuth().put(`/api/friends/${friend.id}`, friend)
+        .then(response => {
+            console.log(response);
+        })
+        .catch(error => {
+            console.log(error);
+        })
+        .finally(() => {
+            setIsEditing(false);
+        });
     }
 
     const deleteFriend = event => {
         event.preventDefault();
         // DELETE here
-        // history over to /friends
+        axiosWithAuth().delete(`/api/friends/${friend.id}`)
+        .then(response => {
+            console.log(response);
+            history.push("/friends");
+        })
+        .catch(error => {
+            console.log(error);
+        })
     }
 
     return(
@@ -41,7 +59,7 @@ const SingleFriend = () => {
                 <SingleFriendView friend={friend} hideView={true} /> }
             <Link to="/friends">Back</Link>
             {isEditing ?                
-                <button onClick={stopEdit}>Save</button> : 
+                <button onClick={saveEdit}>Save</button> : 
                 <button onClick={startEdit}>Edit</button> }
             <button onClick={deleteFriend}>Delete</button>
         </div>
