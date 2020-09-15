2cfc45424a034303b38d00ff64f836c4b1190ca5
Sam Kester <samkester@gmail.com>
2020-09-15 13:56:30 -0600

    App.js routes, prot. routes, context provider

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,24 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Route, Switch } from 'react-router-dom';
+import ProtectedRoute from "./utils/ProtectedRoute";
+import { FriendsContext } from './store/contexts';
 
 function App() {
+  const [friends, setFriends] = useState([]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Switch>
+        <FriendsContext.Provider value={{friends, setFriends}}>
+          <ProtectedRoute path="/friend/:id" component={/* single friend */} />
+          <ProtectedRoute path="/friends" component={/* friends list */} />
+        </FriendsContext.Provider>
+        <Route path="/login" component={/* login */} />
+        <Route path="/" component={/* also login */} />
+      </Switch>
     </div>
   );
 }
--- friends/src/store/contexts.js
+++ friends/src/store/contexts.js
@@ -1,3 +1,3 @@
 import { createContext } from "react";
 
-export const friendsContext = createContext();
+export const FriendsContext = createContext();
